1a601abb27a4f1293931b055e9a82c52f03dd253
jromeo3415 <jromeo3415@floridapoly.edu>
2024-11-16 12:11:31 -0500

    implementing list, download, and upload

--- tcp_file_client.py
+++ tcp_file_client.py
@@ -1,11 +1,21 @@
+#tpc_file_client
 import socket
 
-client_tcp = socket.socket()
-host = '10.200.240.164'
-port = 5001
+s = socket.socket()
+host = '10.200.240.164' #change to the ip address of the host machine
+port = 5001 #change to desired port
 
-try:
-    client_tcp.connect((host, port))
-except:
-    print("Could not establish connection to host. Please check host address and port number.")
-    exit()
+def start_client():
+    try:  #handling unavailable host
+        s.connect((host, port))
+    except:
+        print("Could not establish connection to host. Please check host address and port number.")
+        exit()
+    while True: #infinite loop for commands for server
+        command = input("Enter command (LIST, UPLOAD <filename>, DOWNLOAD <filename>, EXIT): ")
+        s.sendall(command.encode())
+
+
+#allows file to be ran from command line
+if __name__ == "__main__":
+    start_client()
