a715904b6382c1ddc25b1d98de8a5a2fd08da889
jromeo3415 <jromeo3415@floridapoly.edu>
2024-11-16 15:06:43 -0500

    List function working. Need to implement response handling on client

--- tcp_file_client.py
+++ tcp_file_client.py
@@ -2,18 +2,21 @@
 import socket
 
 s = socket.socket()
-host = '10.200.240.164' #change to the ip address of the host machine
-port = 5001 #change to desired port
+host = '34.135.91.16' #change to the ip address of the host machine
+port = 8080 #change to desired port
 
 def start_client():
     try:  #handling unavailable host
         s.connect((host, port))
+        print("Connected to %s on port: %s" %(host, port))
     except:
-        print("Could not establish connection to host. Please check host address and port number.")
+        print("Could not establish connection to host. Ensure server is running and host address and port are correct.")
         exit()
     while True: #infinite loop for commands for server
-        command = input("Enter command (LIST, UPLOAD <filename>, DOWNLOAD <filename>, EXIT): ")
+        command = input("Enter command (LIST, UPLOAD <filename>, DOWNLOAD <filename>, DELETE <filename>, EXIT): ")
         s.sendall(command.encode())
+        response = s.recv(4096).decode()
+        print(response)
 
 
 #allows file to be ran from command line
--- tcp_file_server.py
+++ tcp_file_server.py
@@ -6,23 +6,38 @@ import os
 #creating socket
 s = socket.socket()
 host = '0.0.0.0' #does not need to be changed. allows server to listen on all addresses
-port = 5001 #change value to whatever port you would like to use
+port = 8080 #change value to whatever port you would like to use
 os.makedirs("server_files", exist_ok=True) #directory holding uploaded files
-
+BUFFER = 4096
 def start_server():
-    s.bind((host, port))
-    print("socket binded to %s" % (port))
+    print("Server started")
 
-    s.listen(5)
+    try: #error handling if host cant bind to port or ip
+        s.bind((host, port))
+        print("socket binded to %s" % (port))
+        s.listen(5)
+
+    except:
+        print("Error: Could not bind host to port or address.")
+        exit()
 
     while True:
-        c, addr = s.accept()
-        print('Got connection from ', addr)
+        try:
+            client, addr = s.accept()
+            print('Got connection from ', addr)
+        except:
+            print("Error: Could not accept connection from client")
 
-        c.send('Thanks'.encode())
-        c.close()
-        break
+        #receiving command from client and storing
+        command = client.recv(BUFFER).decode().strip()
+        print(command)
+        if command == "LIST": #client requests list of all files
+            files = os.listdir("server_files")
+            print(files)
+            client.sendall(f"Files available:\n{chr(10).join(files)}\n".encode() if files != "[]" else "No files available.\n".encode())
 
-#allows file to be ran from command line
+
+
+#allows file to be run from command line
 if __name__ == "__main__":
     start_server()
