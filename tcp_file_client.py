937ce0035edccbf369e7f7c5f4851e6bcba50d37
jromeo3415 <jromeo3415@floridapoly.edu>
2024-11-17 14:18:23 -0500

    working on upload, file only partially uploading currently

--- tcp_file_client.py
+++ tcp_file_client.py
@@ -1,22 +1,65 @@
 #tpc_file_client
 import socket
 
-s = socket.socket()
-host = '34.135.91.16' #change to the ip address of the host machine
+s = socket.socket() #socket for client tcp file transfer
+host = '35.225.209.17' #change to the ip address of the host machine
 port = 8080 #change to desired port
+BUFFER = 4096 #packet size
+
 
 def start_client():
     try:  #handling unavailable host
         s.connect((host, port))
         print("Connected to %s on port: %s" %(host, port))
     except:
-        print("Could not establish connection to host. Ensure server is running and host address and port are correct.")
+        print("Error: Could not establish connection to host. Ensure server is running and host address and port are correct.")
         exit()
     while True: #infinite loop for commands for server
-        command = input("Enter command (LIST, UPLOAD <filename>, DOWNLOAD <filename>, DELETE <filename>, EXIT): ")
-        s.sendall(command.encode())
-        response = s.recv(4096).decode()
-        print(response)
+        whole_command = input("Enter command (list, upload <filename>, download <filename>, delete <filename>, subfolder {create|delete} path/directory, EXIT): ")
+
+
+        #client requests list of files
+        if whole_command == "list":
+            s.sendall(whole_command.encode())
+            print(s.recv(BUFFER).decode())
+
+        #client requests to upload file
+        elif whole_command.startswith("upload"):
+            command, file_path = whole_command.split(" ", 2)
+            s.sendall(whole_command.encode())
+            ack = s.recv(BUFFER)
+
+
+            # server is ready to write, send data
+            if(ack == "READY"):
+                with open(file_path, "wb") as folder:
+                    while chunk := (folder.read(BUFFER)):#loop to send packets until full file is sent
+                        s.send(chunk)
+
+            #file is already in server, requests to overwrite
+            elif(ack.startswith("File")):
+                overwrite_choice = input(ack)
+                s.send(overwrite_choice.encode())
+                overwrite_ack = s.recv(BUFFER)
+
+                #client chooses to overwrite
+                if(overwrite_ack == "READY"):
+                    with open(file_path, "wb") as folder:
+                        while chunk := (folder.read(BUFFER)):
+                            s.send(chunk)
+
+                else:
+                    print("Error: Unexpected server ACK")
+
+            else:
+                print("Error: Unexpected server ACK")
+
+
+        elif whole_command.startswith("download"):
+            s.sendall(whole_command.encode())
+
+        else:
+            print("Error: Command not recognized")
 
 
 #allows file to be ran from command line
--- tcp_file_server.py
+++ tcp_file_server.py
@@ -3,12 +3,12 @@ import socket
 import sys
 import os
 
-#creating socket
 s = socket.socket()
 host = '0.0.0.0' #does not need to be changed. allows server to listen on all addresses
 port = 8080 #change value to whatever port you would like to use
 os.makedirs("server_files", exist_ok=True) #directory holding uploaded files
 BUFFER = 4096
+
 def start_server():
     print("Server started")
 
@@ -29,14 +29,49 @@ def start_server():
             print("Error: Could not accept connection from client")
 
         #receiving command from client and storing
-        command = client.recv(BUFFER).decode().strip()
-        print(command)
-        if command == "LIST": #client requests list of all files
+        whole_command = client.recv(BUFFER).decode().strip()
+
+        #client requests list of files
+        if whole_command == "list": #client requests list of all files
             files = os.listdir("server_files")
             print(files)
             client.sendall(f"Files available:\n{chr(10).join(files)}\n".encode() if files != "[]" else "No files available.\n".encode())
 
+        #client requests to upload file
+        elif whole_command.startswith("upload"):
+            command, file_name = whole_command.split(" ",2)
 
+            if(os.path.exists(file_name)): #checking if file is already on server
+                client.sendall("File already exists. Enter 'yes' to overwrite, and 'no' to abort.".encode())
+                overwrite = client.recv(BUFFER).decode().strip() #clients request to overwrite or not
+
+                if(overwrite == "yes"): #client wishes to overwrite duplicate
+                    client.sendall("READY".encode())
+                    file_path = os.path.join("server_files", file_name)
+                    with open(file_path, "wb") as folder: #opening file and replacing contents
+                        while True:
+                            data = client.recv(BUFFER)
+                            if not data:  # stop receiving when data isnt sent
+                                break
+                            folder.write(data)
+
+                elif(overwrite == "no"): #client does not want to overwrite
+                    client.sendall("File not overwritten. Upload aborted")
+                    break
+
+                else: #send error for non-fitting command
+                    client.sendall("Error: Command not recognized")
+                    break
+
+            else: #file doesnt already exist on server, write normally
+                file_path = os.path.join("server_files", file_name)
+                client.sendall("READY".encode())
+                with open(file_path, "wb") as folder: #opening shared directory
+                    while True:
+                        data = client.recv(BUFFER)
+                        if not data: #stop recieving when data isnt recieved
+                            break
+                        folder.write(data)
 
 #allows file to be run from command line
 if __name__ == "__main__":
